b895afb303ba6caf932784d57e3511621b72d380
ya7998 <114100547+ya7998@users.noreply.github.com>
2022-09-22 08:41:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx40ef12767b7b218a",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "5673b2e2fed2e000680c47cde74a4133",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -62,9 +62,9 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "5gzlPgUeEnGACtrwqeCumP09MgRkiDBueH9PzVIveWY",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "5gzlPgUeEnGACtrwqeCumP09MgRkiDBueH9PzVIveWY",
       // 所在省份
       province: "广东",
       // 所在城市
@@ -147,7 +147,7 @@ export const USER_CONFIG = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "5gzlPgUeEnGACtrwqeCumP09MgRkiDBueH9PzVIveWY",
     },
     // 你可以不断按格式往下增加
     // ...
